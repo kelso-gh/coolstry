5d9cf8f1f8deab6e912319b16cf30633e3a54c7f
kelso-gh <soyouwontsingg@gmail.com>
2022-12-17 09:07:12 -0500

    Working on Profile Form

--- src/components/ProfileForm/ProfileForm.jsx
+++ src/components/ProfileForm/ProfileForm.jsx
@@ -1,14 +1,25 @@
 import '../ProfileForm/ProfileForm.css'
+import * as usersAPI from '../../utilities/users-api'
 
-export default function ProfileForm({ user }) {
+
+export default function ProfileForm() {
+    const [users, setUsers] = useState('');
+
+    async function handleSubmit(evt) {
+        evt.preventDefault();
+        const findUser = await usersAPI.getUsersAPI();
+        setUsers(findUser);
+    }
+
     return (
-        <div className="profile-edit">
+        
+            <form className="profile-edit" onSubmit={handleSubmit}>
             <ul>
-                <li>Name: <input type="text" /> </li>
+                <li>Name: <input type="text" required /> </li>
                 <li>Location: <input type="text" /></li>
                 <li>Interests: <input type="text" /></li>
                 <button>Save Changes</button>
             </ul>
-        </div>
+            </form>
     );
 }
